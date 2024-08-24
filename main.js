94d4b44510775d5b6b1c0b54e6ecdb5643682575
Elham <elhamwaheed777@gmail.com>
2024-08-25 04:14:03 +0500

    Responsive

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 menu.onclick = () => {
+  navbar.classList.toggle("open-menu");
   menu.classList.toggle("move");
+};
+// on cilcking one time
+window.onscroll = () => {
+  navbar.classList.remove("open-menu");
+  menu.classList.remove("move");
 };
 // Email
 function validate() {
